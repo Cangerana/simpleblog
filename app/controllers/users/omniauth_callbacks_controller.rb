56469d1d0ec196f9b91ba8386d901768f9884d01
Cangerana <mrjoaovictorc@gmail.com>
2022-12-10 22:00:02 -0300

    Added option to login in application with google

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
+  def google_oauth2
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect @user, event: :authentication
+    end
+  end
+
   # You should also create an action method in this controller like this:
   # def twitter
   # end
@@ -21,7 +30,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super
   # end
 
-  # protected
+  protected
+
+  def after_omniauth_failure_path_for(_scope)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    return new_author_path unless resource_or_scope.author.present?
+    stored_location_for(resource_or_scope) || root_path
+  end
 
   # The path used when OmniAuth fails
   # def after_omniauth_failure_path_for(scope)
